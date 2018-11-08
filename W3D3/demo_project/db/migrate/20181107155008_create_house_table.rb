b474b503afeca85c43c01bc0876ec6ec2d35c40a
Savio Rodrigues <saviofrod@gmail.com>
2018-11-07 22:32:28 -0800

    W3D4 pt 1

--- W3D3/demo_project/db/migrate/20181107155008_create_house_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateHouseTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :house_tables do |t|
-      t.string :address, null: false
-      t.timestamps
-    end
-  end
-end
