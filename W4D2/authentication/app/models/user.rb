9bde0d0caabe52e06bc404d8270bf39dd3f8f043
Savio Rodrigues <saviofrod@gmail.com>
2018-11-14 08:40:17 -0800

    authenticaiton

--- W4D2/authentication/app/models/user.rb
+++ W4D2/authentication/app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ApplicationRecord
+  attr_reader :password
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = Users.find_by(username: username)
+    if BCrypt::Password.new(user.password_digest).is_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
